971318b6fe48e25e0e650c5af9243e455ba74372
MIRARG20 <amiraragab480@gmail.com>
2023-09-07 16:58:58 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,12 @@
 
 /**
  * main - Entry point
- * Description: a c program prints a new line, to the standard error using write function
- *	ssize_t write(int fd, const void *buf, size_t count);
- * Return: 1 (error).
+ * Description: a c program prints a new line, to the standard error
+ * Return: always 1.
 */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
