9aa8ccb6cc0ed2a3ddc18634e00147869b265f5e
MIRARG20 <amiraragab480@gmail.com>
2023-10-25 06:54:20 -0700

    modified

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,8 @@
-Main.h
-
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
+
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -12,4 +11,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif
-
