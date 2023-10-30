1f35c30d25fe9b2dc59e360cf06eab04693fa1a6
MIRARG20 <amiraragab480@gmail.com>
2023-10-30 11:50:38 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,12 @@ char *str_concat(char *s1, char *s2)
 {
 int length1, length2;
 char *ptr;
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL)
 {
 s1 = "";
+}
+if (s2 == NULL)
+{
 s2 = "";
 }
 length1 = strlen(s1);
