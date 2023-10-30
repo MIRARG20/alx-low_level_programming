81d71a67231343ec9bcad36b756b32865980af2b
MIRARG20 <amiraragab480@gmail.com>
2023-10-30 09:34:17 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
 if (size == 0)
 {
 return (NULL);
@@ -19,7 +20,7 @@ if (n == NULL)
 return (NULL);
 else
 {
-for (unsigned int i = 0 ; i < size ; i++)
+for (i = 0 ; i < size ; i++)
 n[i] = c;
 }
 return (n);
