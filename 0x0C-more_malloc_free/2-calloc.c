3613f5bdf667eae61de70ce450240ac8acbc29d6
MIRARG20 <amiraragab480@gmail.com>
2023-11-02 07:40:18 -0700

    test2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * _memset - set a block of memory to a specific value
  * @t: a pointer to the memory block
@@ -8,11 +9,14 @@
  * @num: the number of bytes to be set.
  * Return: pointer
  */
-int _memset(int *t, int value, int num)
+void *_memset(void *t, char value, unsigned int num)
 {
-int i;
+unsigned char *ptr = t;
+unsigned int i;
 for (i = 0; i < num; i++)
-t[i] = value;
+{
+ptr[i] = value;
+}
 return (t);
 }
 
@@ -24,13 +28,16 @@ return (t);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *ptr;
+void *ptr;
 if (nmemb == 0 || size == 0)
+{
 return (NULL);
-ptr = malloc(size * nmemb);
+}
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
+{
 return (NULL);
-_memset(ptr, 0, nmemb);
+}
+_memset(ptr, 0, nmemb * size);
 return (ptr);
-
 }
