8da9638acac1916d584d56bf3b0823b13d5e6115
MIRARG20 <amiraragab480@gmail.com>
2023-11-06 11:03:27 -0800

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: a struct for a dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
