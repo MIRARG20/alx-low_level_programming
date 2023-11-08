921bffd436e1055419a9d83a27ad3d25a70c3dbf
MIRARG20 <amiraragab480@gmail.com>
2023-11-08 11:37:35 -0800

    task3mod

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+
 /**
  * main - program that perfroms simple operations
  * @argc: number of arguments
@@ -8,31 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b;
-char ch;
-int (*func)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-func = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-if (!func)
-{
-printf("Error\n");
-exit(99);
-}
-ch = argv[2];
-if ((ch == '/' || ch == '%') && b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-printf("%d\n", (*func)(a, b));
-return (0);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
 }
